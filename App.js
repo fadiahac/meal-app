941e2c1e5d407fbf772b19c8135ea9184141f121
Ranjan Fadia <kumar.ranjan2525@gmail.com>
2020-09-07 01:48:49 +0530

    meal app basic apploading setup

--- App.js
+++ App.js
@@ -1,8 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
+
+const FetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans-regular': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  })
+}
 
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+  if (!fontLoaded) {
+    return (<AppLoading startAsync={FetchFonts} onFinish={() => setFontLoaded(true)}/>)
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
